f2fc64c72ca2fcc2f23a3e6b14acf384d9aaf9b9
Glitch (flask-app-starter) <none>
2021-02-19 20:19:17 +0000

    🍳💆 Checkpoint ./main.py:14013763/210 ./start.sh:14013763/20

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
-from flask import Flask
+
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/ads-library-data')
+def api_ads_library():
+  return jsonify([{'a':1}])
+
 if __name__ == '__main__':
     app.run(port=int(os.getenv('PORT', 5000)))
+pip
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+import pandas as pd
 python main.py
